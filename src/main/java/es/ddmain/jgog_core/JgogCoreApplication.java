eefa21d319ba372303039761f0b78c1fedb4187a
ddmain <100038906@alumnos.uc3m.es>
2026-08-23 17:40:15 +0200

    Getting Started with Spring Boot (start.spring.io)

---
+++ src/main/java/es/ddmain/jgog_core/JgogCoreApplication.java
@@ -0,0 +1,13 @@
+package es.ddmain.jgog_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JgogCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JgogCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/ddmain/jgog_core/JgogCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ddmain.jgog_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JgogCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
